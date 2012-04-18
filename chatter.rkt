05835a3d1acd94d7c7067dd233d5fa7a76d97bff
Aditya <siddukalluri@gmail.com>
2012-04-17 20:03:31 -0600

    its working!!

--- chatter.rkt
+++ chatter.rkt
@@ -3,42 +3,84 @@
 (define PORT 8090)
 ;(define server-addr "localhost")
 
-
-;(define (client-loop)
-;  (define (loop)
-;    (display "Chat box will be displayed\n"))
-;  (loop)
-;  )
-    
-
-
+(define listening-thread null)
+           
 ;Here the user joins
 (define (new-connection host port uname)
-  ;TO-DO Create a  thread for the user 
-  ;Connecting to the server here
+   ;Connecting to the server here
   (define-values (in out) (tcp-connect host port))
-  
   (send join-dialog show #f)
-  (thread (lambda() ;(client-loop)
-           (file-stream-buffer-mode out 'none)
-           (display "Chat box will be displayed\n") 
-            ;Some Header here
-            ;Sending the user name
-            ;(write-bytes (string->bytes/locale uname) out)
-            (write-bytes (string->bytes/locale uname) out)
-             
-            ;(if (equal? (read in) "ack") (display "success") (display "no ACK"))
-            (if (equal? (read-bytes 3 in) #"ack") (display "success") (display "no ACK"))
-            ;(send select-dialog show #f)
-            
-            )) 
+  (file-stream-buffer-mode out 'none)
+  (display "Chat box will be displayed\n") 
+  ;sending signin message to the server
+  (display (string? uname))
+  (write `(signin ,uname) out)
   
-  ;Some Header here
-  ;Sending the user name
-  ;(print uname out)
-  ;(if (equal? (read in) "ack") (display "success") (display "no ACK")
-      )
- 
+  
+  (define (loop)
+    (define event (thread-receive-evt));you are sending messagea
+    (define receive (sync event in))
+    (if (equal? event receive) (broadcast-message out)
+      (receive-broadcast in out));receive a message from server
+    (loop))
+  
+   (send main-win show #t)
+  
+   (set! listening-thread (thread (lambda ()
+                                    (loop)))))
+                     
+            
+
+
+(define (broadcast-message out)
+  (define msg (thread-try-receive));will get #f or message
+  (display "\nEntered :")(display msg)
+  ;(write (cons 'comm msg) out)
+  (write (list 'broadcast msg) out)
+  ;(write-bytes (string->bytes/locale msg) out)
+  (display "\nbroadcasted\n"))
+
+(define (read-message in)
+ ; (let ([buff (read-bytes-line in 'return)])
+  ;  (if (eof-object? buff) #f
+   ;   (bytes->string/utf-8 buff))
+  
+   ; )
+  (read in)
+  )
+
+
+
+(define (add-to-chat msg out)
+   (send chatbox set-value (string-append (send chatbox get-value) msg "\n"))
+   (display (string-append "Received message :" msg "\n"))
+    (write (list 'listofusers) out))
+
+(define (refresh-list l)
+  (for ([i l]) (display i)))
+
+(define (receive-broadcast in out)
+  (display "Receiving from server\n")
+  ;try to do the header extraction
+  (define msg (read-message in))
+  (display msg)
+  (match msg
+    [(list 'signin a) (handle-error a)]
+    [(list 'broadcast b) (add-to-chat b out)]
+    [(list 'listofusers c) (refresh-list c)]
+    [eof-object (void)]
+    [_ (error "Unexpected!!!!!")]))
+   
+  
+;===================================================
+;Dialog box for error
+(define (handle-error msg)
+(define error-dialog (instantiate dialog% (msg)))
+  (send error-dialog show #t)
+  (send name-field set-value ""))
+
+
+
 ;===================================================
 ;Dialog box for select
 (define select-dialog (instantiate dialog% ("Select type of chat")))
@@ -54,7 +96,46 @@
                   ;TO-DO ERROR CHECKING AND USER NAME VALIDATION using with-handlers
                                            (new-connection (send host-field get-value) PORT (send name-field get-value)))))
  
+;===================================================
+;Dialog box for chat/broadcast
 
+
+(define (textbox-changed t e)
+  (and (eq? (send e get-event-type) 'text-field-enter)
+       (let ([msg (send t get-value)])
+         (display msg)
+         ; Queue the message for sending
+         (thread-send listening-thread msg)
+         ; Clear the textbox
+         (send t set-value ""))))
+
+(define main-win  (new frame%
+                        [label "ChatRoom"]
+                        [width 600]
+                        [height 400]
+                        [min-width 200]
+                        [min-height 200]
+                        ))
+;(define user-panel (new vertical-panel% [parent main-win]
+ ;                     [alignment '(right right)]))
+
+; The box that holds the logged in users
+;(define users-list-box (new list-box%
+;                            [parent main-win]
+;                            [choices (get-users)]
+;                            [label "List of users"]
+;                            ))
+
+(define chatbox (new text-field%
+                     [parent main-win]
+                     [label "Conversation"]
+                     [min-height 300]
+                     [enabled #f]))
+(define textbox (new text-field%
+                     [parent main-win]
+                     [label "Send a message:"]
+                     [callback textbox-changed]
+                            ))
 
 
 
@@ -80,8 +161,8 @@
 (new button% [parent panel] [label "Connect"]
      (callback (lambda (button event)
                   ;TO-DO ERROR CHECKING AND USER NAME VALIDATION using with-handlers
-                                        ;    (new-connection (send host-field get-value) PORT (send name-field get-value))
-                 (send select-dialog show #t))))
+                  (new-connection (send host-field get-value) PORT (send name-field get-value)))))
+                 ;(send select-dialog show #t))))
 (new button% [parent panel] [label "Cancel"])
 
 ;*****START*********
---
+++ server_scratch.rkt
@@ -0,0 +1,179 @@
+;Lambda Chat Server
+#lang racket
+
+(require racket/date)
+(require racket/set)
+
+;Global Constants
+(define HOSTNAME #f)    ;Host is the localhost
+(define PORT 8090)      ;Port is some convenient
+(define REUSE-PORT #t)  ;For Debugging
+(define MAX 10)         ;Max number of clients on the waiting Q
+
+;Main Function
+(define (server port)
+  
+  (define users (make-hash))    ;Mapping from the user to the socket
+  (define user-threads (set))   ;A set of user Threads so the server can broadcast
+  
+  (define (reg-user username in out)
+    (hash-set! users username (cons in out)))
+  
+  (define (unreg-user username)
+    (and (hash-has-key? users username) (hash-remove! users username)))
+  
+  (define (reg-thread thr)
+    (set! user-threads (set-add user-threads thr))
+    )
+  
+  (define (unreg-thread thr)
+    (set! user-threads (set-remove user-threads thr)))
+  
+  (define (broadcast cmd msg)
+    (define sendIt `(,cmd ,msg)) 
+    (display "Inside broadcast\n")
+    (display sendIt)
+    (set-for-each user-threads (lambda (t) (thread-send t sendIt)))
+    ;(hash-map users (lambda (_ v) (write `(,cmd ,msg) (second v))))
+    )
+  
+  (define (list-of-users)
+    (hash-map users (lambda (u _) u)))
+  
+  (define listener
+    (begin
+      (display "Starting the server\n")
+      (let ([res (tcp-listen port MAX REUSE-PORT HOSTNAME)])
+      (display (string-append "Server waiting for clients at port " (number->string port) ".\n"))
+        res
+        )
+      )
+    )
+  
+  (define (handle-client listener)
+    (define-values (in out)
+                   (tcp-accept listener))
+    (thread (lambda ()
+              (reg-thread (current-thread))           ;Register the thread
+              (file-stream-buffer-mode out 'none)     ;Cancel the buffer. No delay.
+              (communicate in out)                    ;Actual Thread communication happens
+              (display "Connection ended. Removing thread...")
+              (unreg-thread (current-thread))
+              (close-input-port in)
+              (close-output-port out)
+              )
+            )
+    )
+  
+  (define (communicate in out)
+    
+    ;Provides the functions to set and get the username. Initially the username is set to null
+    (define-values (get-user set-user)
+                   (let ((user null)) 
+                     (define (get) user)
+                     (define (set username) (set! user username))
+                     (values get set)
+                     ))
+    
+    ;Signin the user
+    (define (signin username in out)
+      (define (post-action)
+        (display "\ninside post action of signin\n ")
+        (broadcast 'broadcast (string-append username " has joined"))
+        )
+      (display (string-append "Signup : " username))
+      (if (hash-has-key? users username)
+          (write (cons 'signin-samename (string-append username " -username has been taken")))
+          (begin
+            (reg-user username in out)
+            (set-user username)
+            (display (string-append (number->string (hash-count users)) " users online"))
+            (post-action)
+            )
+          )
+      )
+    
+    ;Signout the user
+    (define (signout username in out)
+      (define (post-action)
+        (broadcast 'broadcast (string-append username " has signed out"))
+        )
+      (display (string-append "Trying to sign out the user" username))
+      (if (not (hash-has-key? users username))
+          (display (string-append "No user named " username " to sing out"))
+          (begin
+            (unreg-user(username))
+            (display (string-append username " is signed out"))
+            (post-action)
+            )
+          )
+      )
+    
+    ;return the list of users
+    (define (userlist in out)
+      (define msg (cons 'listofusers (list-of-users)))
+      (display "Returning the list of users")
+      )
+    
+    ;;Handle the events by looping
+    (define (communicate-loop)
+      (display "Looping in the communicate-loop\n")
+      (let* ([msg-evt (thread-receive-evt)]
+             [ready (sync msg-evt in)])
+        (if (eq? ready msg-evt)
+          (handle-push)
+          (handle-input ready)
+          )
+        )
+      )
+    
+    
+    (define (handle-push)
+     (display "Pushing...\n")
+      (let ([instruction (thread-try-receive)])
+        ; Send push only if user is logged in
+        (display instruction)
+        (and (get-user) instruction (begin
+                                      ( write instruction out)
+                                      )
+             (communicate-loop)
+        ) 
+        )
+      )
+    
+    ;;Handle the input
+    (define (handle-input in)
+      (define message (read in))
+      (define id (first message))
+      (cond
+        [(eq? id 'signin ) (signin (second message) in out)]
+        [(eq? id 'signout) (signout (second message) in out)]
+        [(eq? id 'listofusers) (userlist in out )]
+        [(eq? id 'broadcast) (broadcast 'broadcast (string-append (get-user) "says : "(second message)))]
+        [else (begin (display id) (display "\n") (display (second message)) (display "\n")  (display (third message)) (display "\n") (display message))]
+        )
+      (communicate-loop)
+      )
+    
+    ;The thread is quiting so remove the user from the user name and remove the thread
+    (define (unreg)
+      (unreg-thread (current-thread))
+      (unreg-user (get-user))
+      )
+    
+    ;The starting point of the communicate with the exception handlers for exception
+    (with-handlers ([(lambda (e) #t) (lambda (e) (unreg))])
+                   (communicate-loop))
+    
+    
+    )
+
+  (define (main)
+    (handle-client listener)
+    (main))
+  
+  (main)
+  
+ )
+
+(server PORT)
